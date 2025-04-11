f3a524b360c5e47e0d96fbc805fbfa5bb9436ffb
caiovitoraf <caiovitor2506@gmail.com>
2025-04-11 11:24:17 -0300

    tailwind imported

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
@@ -6,3 +7,7 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+=======
+# ListManager
+This to-do list web application was developed step-by-step by following a guided React tutorial available on YouTube: https://www.youtube.com/watch?v=2RWsLmu8yVc. The project demonstrates core React concepts such as components, state, and event handling.
+>>>>>>> a3acbbc0922aeaeccf9f21d487cec3c1f166311d
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+  const [tasks, setTasks] = useState([
+    {
+    id: 1,
+    title: "Estudar programação",
+    description: "Estudar programação para se tornar um desenvolvedor full stack",
+    isCompleted: false
+    },
+    {
+    id: 2,
+    title: "Estudar inglês",
+    description: "Estudar inglês para conseguir um emprego melhor",
+    isCompleted: false
+    },
+    {
+    id: 3,
+    title: "Estudar matemática",
+    description: "Estudar matemática para passar no vestibular",
+    isCompleted: false
+  },
+]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text -3xl text-slate-100 font-bold-center">
+          Gerenciador de Tarefas</h1>
+        <Tasks tasks={tasks}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
